9cf558140018f5f40c7a52e87e26449ad8a27704
Buck Ryan <buck.ryan2@gmail.com>
2017-10-14 15:12:15 -0400

    Version 0.3.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="dbrhino-agent",
-    version="0.3.1",
+    version="0.3.2",
     description="Agent for dbrhino",
     author="Buck Ryan",
     url="https://dbrhino.com",
